aa41f41bae0c11b1b327fdde1ad91c94ca4330e9
Shitansu7205 <shitansukumargochhayat@gmail.com>
2025-04-19 01:49:20 +0530

    Adding some changes like footer and header with Vision and mission

---
+++ footer.js
@@ -0,0 +1,124 @@
+document.write(`
+    
+    <footer class="footer-wrapper footer-layout2 bg-transparent">
+        <div class="widget-area">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-md-6 col-xxl-3 col-xl-4">
+                        <div class="widget footer-widget">
+                            <div class="th-widget-about">
+                                <div class="about-logo">
+                                    <a href="index.html"><img src="assets/my-img/logo.png" alt="Webteck" style="width: 160px;"></a>
+                                </div>
+                                <p class="about-text">Professionally redefine transparent ROI through low-risk
+                                    high-yield imperatives. Progressively create empowered. cost effective users via
+                                    team driven.</p>
+                                <div class="th-social">
+                                    <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+                                    <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                                    <a href="https://www.whatsapp.com/"><i class="fab fa-whatsapp"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-auto">
+                        <div class="widget widget_nav_menu footer-widget">
+                            <h3 class="widget_title">Quick Links</h3>
+                            <div class="menu-all-pages-container">
+                                  <ul>
+                      <li class=" mega-menu-wrap">
+                          <a href="index.html">Home</a>
+                      </li>
+                      <li><a href="index.html#about">About Us</a></li>
+                      <li class="">
+                          <a href="index.html#service">Services</a>
+                      </li>
+                      <li class="">
+                          <a href="index.html#mission">Mission & Vision</a>
+                      </li>
+                      <li class="">
+                          <a href="index.html#founder">Our Founder</a>
+                      </li>
+                      <li>
+                          <a href="index.html#contact">Contact</a>
+                      </li>
+                  </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-auto">
+                        <div class="widget footer-widget">
+                            <h3 class="widget_title">Contact Us</h3>
+                            <div class="th-widget-contact">
+                                <div class="contact-feature">
+                                    <div class="icon-btn">
+                                        <i class="fa-solid fa-phone"></i>
+                                    </div>
+                                    <div class="media-body">
+                                        <p class="contact-feature_label">Phone Number</p>
+                                        <a href="tel:+918144836533" class="contact-feature_link">+91 8144836533</a>
+                                    </div>
+                                </div>
+                                <div class="contact-feature">
+                                    <div class="icon-btn">
+                                        <i class="fa-solid fa-envelope"></i>
+                                    </div>
+                                    <div class="media-body">
+                                        <p class="contact-feature_label">Email address</p>
+                                        <a href="mailto:tripstar.in1@gmail.com"
+                                            class="contact-feature_link">tripstar.in1@gmail.com</a>
+                                    </div>
+                                </div>
+                                <div class="contact-feature">
+                                    <div class="icon-btn">
+                                        <i class="fa-solid fa-location-dot"></i>
+                                    </div>
+                                    <div class="media-body">
+                                        <p class="contact-feature_label">office location</p>
+                                        <a href="https://www.google.com/maps" class="contact-feature_link">Odisha,
+                                            India</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-auto">
+                        <div class="widget footer-widget">
+                            <h3 class="widget_title">Our Location</h3>
+                            <div class="recent-post-wrap">
+                                    <div class="recent-post">
+                                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d239487.1652253839!2d85.65564125231477!3d20.300807016970502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909d2d5170aa5%3A0xfc580e2b68b33fa8!2sBhubaneswar%2C%20Odisha!5e0!3m2!1sen!2sin!4v1745004337353!5m2!1sen!2sin" width="200" height="200" style="border-radius:10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-wrap bg-theme">
+            <div class="container">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-lg-6">
+                        <p class="copyright-text">Copyright <i class="fal fa-copyright"></i> 2025 Trip Star || All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 text-end d-none d-lg-block">
+                        <div class="footer-links">
+<p class="copyright-text">Powered by   <a
+                                href="https://thetechtide.site"><img src="assets/my-img/techtide-logo.jpeg" alt="logo" style="width: 100px; object-fit: cover;"></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="shape-mockup" data-top="0%" data-left="0%"><img src="assets/img/shape/footer_shape_3.png"
+                alt="shape"></div>
+        <div class="shape-mockup" data-top="0%" data-right="0%"><img src="assets/img/shape/footer_shape_4.png"
+                alt="shape"></div>
+        <div class="shape-mockup" data-bottom="0%" data-left="0%"><img src="assets/img/shape/footer_shape_5.png"
+                alt="shape"></div>
+        <div class="shape-mockup" data-bottom="0%" data-right="0%"><img src="assets/img/shape/footer_shape_6.png"
+                alt="shape"></div>
+    </footer>
+    
+    `)
